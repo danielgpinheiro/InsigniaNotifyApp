3ce73c145d4d096d8617ad44899086fc51713926
Daniel Pinheiro <dedelabritos@gmail.com>
2023-10-13 14:17:04 -0300

    Fix no pattern matching when POST request returns 404

--- lib/insignia_notify_app_web/controllers/http/api.ex
+++ lib/insignia_notify_app_web/controllers/http/api.ex
@@ -19,6 +19,9 @@ defmodule InsigniaNotifyAppWeb.Http.Api do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         HandleResponse.response(:ok, body)
 
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        HandleResponse.response(:error, :not_found)
+
       {:error, %HTTPoison.Error{reason: reason}} ->
         HandleResponse.response(:error, reason)
     end
